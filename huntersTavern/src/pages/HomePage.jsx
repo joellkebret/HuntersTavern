26f9d29f78a9390c0e010c281b535bfe66091a69
jayqss <joellkebret05@gmail.com>
2025-01-19 21:13:44 -0500

    V1.1.5 added welcome message for hero

--- huntersTavern/src/pages/HomePage.jsx
+++ huntersTavern/src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import '../styles/HomePage.css';
 
 function HomePage() {
-  return (
-    <h1>home page</h1>
-  )
-}
+    return (
+      <div className='WelcomeContainer'>
+          <div className='welcomeImg'>
+              <img src='./welcomeImg.jpg' alt='Welcome Image' />
+          </div>
+          <div className='welcomeMsg'>
+              <h1>
+                  Welcome to The Hunter's Tavern! Your ultimate Hunter x Hunter sanctuary, where fans gather to explore deep lore, lively forums, and the latest buzz from the web. Dive into dedicated sections for anime and manga, and relive your favorite moments. Enter The Hunter's Tavern, where your next Hunter x Hunter discovery awaits!
+              </h1>
+          </div>
+      </div>
+    );
+  }
 
-export default HomePage
+export default HomePage;
