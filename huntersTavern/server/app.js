b8976cad9d3e6b7eeef82f639227721ec4413670
jayqss <joellkebret05@gmail.com>
2025-01-29 17:01:13 -0500

    Hosted on vercel (local only)

--- huntersTavern/server/app.js
+++ huntersTavern/server/app.js
@@ -54,7 +54,7 @@ app.post("/Sign-up", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 7001;
+const PORT = 7001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
